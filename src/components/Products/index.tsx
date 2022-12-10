b69e5570924932984d6800345848f2eb8ae2c7c0
TAlam <tanvir.alam.shawn@gmail.com>
2022-12-10 03:23:08 +0100

    spliting products

--- src/components/Products/index.tsx
+++ src/components/Products/index.tsx
@@ -4,26 +4,82 @@ import ProductCard from "./ProductCard";
 export default function ProductFeeds({ products }: Products) {
   return (
     <div className="mx-auto grid grid-flow-row-dense md:-mt-52 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-      {products.map(
-        ({
-          id,
-          title,
-          price,
-          description,
-          category,
-          image,
-        }: Products["products"]) => (
-          <ProductCard
-            key={id}
-            id={id}
-            title={title}
-            price={price}
-            description={description}
-            category={category}
-            image={image}
-          />
-        )
-      )}
+      {products
+        .slice(0, 3)
+        .map(
+          ({
+            id,
+            title,
+            price,
+            description,
+            category,
+            image,
+          }: Products["products"]) => (
+            <ProductCard
+              key={id}
+              id={id}
+              title={title}
+              price={price}
+              description={description}
+              category={category}
+              image={image}
+            />
+          )
+        )}
+
+      <img
+        className="md:col-span-full"
+        src="https://links.papareact.com/dyz"
+        alt=""
+      />
+
+      <div className="md:col-span-2">
+        {products
+          .slice(4, 5)
+          .map(
+            ({
+              id,
+              title,
+              price,
+              description,
+              category,
+              image,
+            }: Products["products"]) => (
+              <ProductCard
+                key={id}
+                id={id}
+                title={title}
+                price={price}
+                description={description}
+                category={category}
+                image={image}
+              />
+            )
+          )}
+      </div>
+
+      {products
+        .slice(5, products.length)
+        .map(
+          ({
+            id,
+            title,
+            price,
+            description,
+            category,
+            image,
+          }: Products["products"]) => (
+            <ProductCard
+              key={id}
+              id={id}
+              title={title}
+              price={price}
+              description={description}
+              category={category}
+              image={image}
+            />
+          )
+        )}
     </div>
   );
 }
